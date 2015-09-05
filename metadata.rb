61664e6d09251c9bb39c6194edb5161f4b56c156
Tim Smith <tsmith84@gmail.com>
2015-09-04 18:48:56 -0700

    Update maintainer email and add chef 12 metadata

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,11 @@
 name             'client-rekey'
 maintainer       'Chef Software, Inc.'
-maintainer_email 'dev@getchef.com'
+maintainer_email 'cookbooks@chef.io'
 license          'Apache 2.0'
 description      'Regenerates your client key'
 long_description 'Regenerates your client key-recommended for users affected by heartbleed'
 version          '0.1.0'
+
+source_url 'https://github.com/opscode-cookbooks/client-rekey' if respond_to?(:source_url)
+issues_url 'https://github.com/opscode-cookbooks/client-rekey/issues' if respond_to?(:issues_url)
 
