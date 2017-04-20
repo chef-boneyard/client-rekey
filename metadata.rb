64e993d2b17b858d0013383ae4b00ae9bcfd9a6c
Tim Smith <tsmith@chef.io>
2017-04-20 14:21:42 -0700

    Use a standardized license string in the metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'client-rekey'
 maintainer 'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'Regenerates your client key'
 long_description 'Regenerates your client key-recommended for users affected by heartbleed'
 version '0.2.0'
